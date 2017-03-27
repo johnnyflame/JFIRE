1e264b15ef6b07c6f11b7929f8b2f41c323bd7c7
johnnyflame <johnnyflamelee@gmail.com>
2017-03-28 10:43:22 +1300

    Initialized repo.

---
+++ ass1/JFIRE/src/parser/Parser.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+/**
+ *
+ * @author Johnny Flame Lee 2017
+ */
+public class Parser {
+    
+}
