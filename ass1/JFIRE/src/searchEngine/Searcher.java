b94096ce352fb2a174c5b37d83bc826a3cf61827
johnnyflame <johnnyflamelee@gmail.com>
2017-04-20 16:13:45 +1200

    Initialized repo.

--- ass1/JFIRE/src/searchEngine/Searcher.java
+++ ass1/JFIRE/src/searchEngine/Searcher.java
@@ -7,6 +7,8 @@ package searchEngine;
 
 import java.io.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.*;
 
 /**
@@ -18,23 +20,66 @@ import java.util.zip.*;
 public class Searcher {
     public static HashMap<Integer,String> dictionary;
     public static HashMap<String,ArrayList<Posting>> invertedIndex;
+    public static HashMap<String,long[]> metaData;
     
     public static ArrayList<String> queryList;
     
     public static void main(String [] args){
-     //   queryList = parseQuery();
- 
+        
+        metaData = deserializeData("./data/metaData");
+        
+        queryList = parseQuery();
+        lookUp(queryList,metaData);
 
+        
     }
     
-    public static void lookUp(ArrayList<String> queries){
+    public static void readRandomAccessFile(String filePath, long pos, long size) throws IOException{
+        int [] docIDs = new int [size];
+        int [] frequencies = new int [size];
+        
+        
+        RandomAccessFile file = new RandomAccessFile(filePath, "r");
+        
+        file.seek(pos);
+        
+        for(int i = 0; i < size;i++){
+            docIDs[i] = file.readInt();
+            frequencies[i] = file.readInt();
+        }
+        
+        for(int i = 0; i < size;i++){
+            System.out.println("docID: " + docIDs[i] + "Frequencies: " + frequencies[i]);
+        }
+       
+        
+    }
+    
+    public static void lookUp(ArrayList<String> queries,HashMap<String,long[]> metaData){
+      
+        
         for (String s:queryList){
-            //retrieve results here
+            System.out.println("You searched for: " + s);
+            
+            if (!metaData.containsKey(s)){
+                System.out.println("Term not found");
+                return;
+            }else{
+                try {
+                    long pos = metaData.get(s)[0];
+                    long size = metaData.get(s)[1];
+                    
+                    readRandomAccessFile("./data/index.dat",pos,size);
+                } catch (IOException ex) {
+                    Logger.getLogger(Searcher.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
         }
     }
+    }
     
-    public static HashMap<String,ArrayList<Posting>> deserializeData(String name){
-        String filename = name + ".ser";
+    public static HashMap<String,long[]> deserializeData(String name){
+        String filename = name + ".dat";
         HashMap<String,ArrayList<Posting>> dict = new HashMap<>();
         try{
             
@@ -42,12 +87,12 @@ public class Searcher {
             GZIPInputStream gs = new GZIPInputStream(fis);
             ObjectInputStream ois = new ObjectInputStream(gs);
             
-             dict = (HashMap)ois.readObject();
+            dict = (HashMap)ois.readObject();
             
             ois.close();
             fis.close();
         }catch(IOException e){
-            e.printStackTrace();
+            System.out.println("term not in collection");
         }catch(ClassNotFoundException c){
             System.out.println("Class not found.");
             c.printStackTrace();
